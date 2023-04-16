e1a7a918bf632220d9c61c14bd48b75bcc86b177
Kássia <kassia_guterres@outlook.com>
2023-04-16 18:05:02 -0300

    chore: implemented encrypted password

--- balance-app-back/src/user/user.service.ts
+++ balance-app-back/src/user/user.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { Prisma } from '@prisma/client';
+import * as bcrypt from "bcrypt";
 @Injectable()
 export class UserService {
-  constructor(private readonly: PrismaService){}
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  
+  constructor(private prisma: PrismaService){}
+ 
+  async create(createUserDto: CreateUserDto) {
+
+  const data = {
+    ...createUserDto,
+    password: await bcrypt.hash(createUserDto.password, 10),
+  };
+
+    const createdUser = await this.prisma.user.create({
+      data
+    });
+
+    return {
+      ...createdUser,
+      password:undefined,      
+    };
   }
 
   // findAll() {
