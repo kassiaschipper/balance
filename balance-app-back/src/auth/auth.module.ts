0d4c62988677879b6146914209a5e10d4908babe
Kássia <kassia_guterres@outlook.com>
2023-04-17 00:58:17 -0300

    feat auth: implemented login validation

--- balance-app-back/src/auth/auth.module.ts
+++ balance-app-back/src/auth/auth.module.ts
@@ -1,17 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategy/local.stategy';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { LoginValidationMiddleware } from './middlewares/login-validation.middleware';
 
 @Module({
   imports:[UserModule, JwtModule.register({
     secret: process.env.JWT_SECRET,
-    signOptions:{expiresIn: '12m'}
+    signOptions:{expiresIn: '30d'}
   })],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
  })
-export class AuthModule {}
+export class AuthModule implements NestModule{
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoginValidationMiddleware).forRoutes('login');
+    }
+}
