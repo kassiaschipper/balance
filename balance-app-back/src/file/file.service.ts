d3a79e96798f2220bb1512fe4d4abe4215561e08
Kássia <kassia_guterres@outlook.com>
2023-04-18 10:44:10 -0300

    feat home: implemented balance sum by spf and date

--- balance-app-back/src/file/file.service.ts
+++ balance-app-back/src/file/file.service.ts
@@ -13,9 +13,19 @@ export class FileService {
             ...createFileDto,
         };
         const createFile = await this.prisma.file_data.create({ data });
-
+        
         return {
             createFile
         }
+    }
+
+    async getBalanceSumByCpf(){
+        const sum = await this.prisma.file_data.groupBy({
+            by: ["cpf", "sate"],
+            _sum:{balance:true}
+        });
+        
+        return sum;   
+
     }
 }
